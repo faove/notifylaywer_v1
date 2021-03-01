7f2ba08d6c7e469f25adeb67f69a90c7b3e88e87
Francisco Alvarez <faove@hotmail.com>
2021-02-28 22:15:44 -0300

    add ServicesController

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::group(['middleware' => ['cors']], function () {
     Route::get('/api/services', [ServicesController::class, 'index']);
     Route::get('api/gets/{userid}', [UserController::class, 'index']);
     Route::get('api/clients/{clientid}', [ClientController::class, 'index']);
+    Route::get('/api/services/{serviceid}', [ServicesController::class, 'index']);
     Route::get('api/associates/{associateid}',[AssociatesController::class, 'index']);
 });
 

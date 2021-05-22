38fa6eb137b4339e1cacd72655d999476544ddad
Francisco Alvarez <faove@hotmail.com>
2021-05-22 15:39:30 -0300

    Save Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,14 +15,17 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
+        return next($request)
+        // Establecemos la cabecera CORS para permitir el origen de solicitud desde localhost
+        ->header('Access-Control-Allow-Origin', '*');
+        // $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD');
-        $response->headers->set('Access-Control-Allow-Headers',
-            'Content-Type, Accept, Authorization, X-Requested-With, Application');
+        // $response->headers->set('Access-Control-Allow-Origin', '*');
+        // $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD');
+        // $response->headers->set('Access-Control-Allow-Headers',
+        //     'Content-Type, Accept, Authorization, X-Requested-With, Application');
 
-        return $response;
+        // return $response;
         // return $next($request);
     }
 }

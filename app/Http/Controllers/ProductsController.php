cd73d5d47fbb9259db0e708724cbfee8b4790e09
Francisco Alvarez <faove@hotmail.com>
2021-08-03 23:19:04 -0300

    Comentado notificacion por correo

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -148,9 +148,9 @@ class ProductsController extends Controller
                 // field: 'name', 
                 // field: 'date_start', 
                 // field: 'date_end', 
-                Mail::to(User::find(1)->email)->later(5,
-                    new SentProduct([], "{$this->description}",
-                        "El proceso {$this->description} se bloquea: {$e->getMessage()} . ':: Linea' . {$e->getLine()}"));
+                // Mail::to(User::find(1)->email)->later(5,
+                //     new SentProduct([], "{$this->description}",
+                //         "El proceso {$this->description} se bloquea: {$e->getMessage()} . ':: Linea' . {$e->getLine()}"));
                 
                     return json_encode($result_product);
 

4c2efd8aa36272120b09343fef75871137b2504c
Francisco Alvarez <faove@hotmail.com>
2023-05-27 12:08:11 -0300

    NL-002: Fix

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,9 +15,10 @@ class UserSeeder extends Seeder
     {
         $users = array(
             ['id' => 1, 'name' => 'Administrador', 'email' => 'faovenezuela@gmail.com', 'password' => Hash::make('adminadmin')],
-            ['id' => 2, 'name' => 'Administrador', 'email' => 'npino34@gmail.com', 'password' => Hash::make('npino34')],
-            ['id' => 3, 'name' => 'Anna', 'email' => 'anna@gmail.com', 'password' => Hash::make('notifylaywer')],
-            ['id' => 4, 'name' => 'Helen Pino', 'email' => 'helenpino@gmail.com', 'password' => Hash::make('notifylaywer')],
+            ['id' => 2, 'name' => 'Administrador', 'email' => 'vm.tonello@gmail.com', 'password' => Hash::make('adminadmin')],
+            ['id' => 3, 'name' => 'Nelson Pino', 'email' => 'npino34@gmail.com', 'password' => Hash::make('npino34')],
+            ['id' => 4, 'name' => 'Anna', 'email' => 'anna@gmail.com', 'password' => Hash::make('notifylaywer')],
+            ['id' => 5, 'name' => 'Helen Pino', 'email' => 'helenpino@gmail.com', 'password' => Hash::make('notifylaywer')],
         );
 
         // Loop through each user above and create the record for them in the database
